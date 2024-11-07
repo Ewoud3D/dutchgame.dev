0d7d24af4f4272694bba1ba29c757d87d88eeeec
Ewoud <github@ewoudva.nderwerf.com>
2024-11-07 11:27:18 +0100

    Added feed from Instagram (temporary own instagram)

---
+++ public/js/instagram-feed.js
@@ -0,0 +1,36 @@
+var feed = document.getElementById('feed');
+
+RefreshFeed();
+
+async function RefreshFeed()
+{
+	const url = "https://feeds.behold.so/5bVQOJtAAMkPv5AyUDx8";
+	try
+	{
+		const response = await fetch(url);
+		if (!response.ok)
+		{
+			throw new Error(`Response status: ${response.status}`);
+		}
+
+		const data = await response.json();
+
+		var count = data.posts.length;
+		if (count > 3)
+			count = 3;
+
+		feed.innerHTML = '';
+
+		for (let i = 0; i < count; i++)
+		{
+			feed.innerHTML += 
+				`<a class="col-sm-6 col-lg-4 news-item" href="${data.posts[i].permalink}" target="_blank">
+					<img src="${data.posts[i].thumbnailUrl}"/>
+				</a>`;
+		} 
+	}
+	catch (error)
+	{
+		console.error(error.message);
+	}
+}
